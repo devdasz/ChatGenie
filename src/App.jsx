60223de8bba64c77921eae22f9600f4eb4ddcdad
devdazz <14913748-devdazz@users.noreply.replit.com>
2023-05-08 02:35:11 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+
+      <div class="flex items-start space-x-4 p-4">
+        <img class="w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1683143724719-2dc07dc1fe31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=992&q=80" alt="" />
+   
+        <div class="font-medium bg-gray-600 p-2 rounded-lg text-white">
+
+          <div class="text-sm "> Hi I am A auyeffeifwiewifgweigw wigwigiefjb iuhfuiwefwe efiequfiufhwief iodvoijeijwefjefwe svijepofpefepfoepofkepfw woifjwijfwf wiweoifjwejfwiejf jwfwefwepfwepfj fjfwjpwejf oifjifjiewfjpw iwjfpewjpwefpweo igirgiwrwe  </div>
+        </div>
+      </div>
+
+    </main>
+  )
+}
